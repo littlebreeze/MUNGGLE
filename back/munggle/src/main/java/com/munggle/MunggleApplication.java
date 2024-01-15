70cdd8fbb5f54be1903ab337990b892f0f5e8f35
이형우 <dlguddn3024@gmail.com>
2024-01-15 12:49:07 +0900

    :tada:[BE]feat: 프로젝트 세팅

---
+++ back/munggle/src/main/java/com/munggle/MunggleApplication.java
@@ -0,0 +1,13 @@
+package com.munggle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunggleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunggleApplication.class, args);
+	}
+
+}
---
+++ back/munggle/src/test/java/com/munggle/MunggleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.munggle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunggleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
