06de652d5f77a9248dd0d91bc06be03caa1a8cbb
이형우 <dlguddn3024@gmail.com>
2024-01-16 09:36:11 +0900

    :sparkles: feat: 보안적인 애플리케이션 관리를 위한 SecurityConfig 구현

---
+++ back/munggle/src/main/java/com/munggle/security/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.munggle.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .csrf().disable()
+                .authorizeRequests()
+                .requestMatchers(
+                        request -> request.getServletPath()
+                                .startsWith("/member/join")).permitAll()
+                .anyRequest().authenticated();
+
+        return httpSecurity.build();
+    }
+}
