0fc54338e11ca589c1d4481f78c5bd63c32b731b
이지원 <dlwldnjs950@naver.com>
2024-01-17 10:27:58 +0900

    :fire: [BE] remove : 반려동물 동반 시설 관련 코드 삭제

--- back/munggle/src/main/java/com/munggle/openAPI/repository/HospitalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.munggle.openAPI.repository;
-
-import com.munggle.domain.model.entity.Hospital;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
-}
