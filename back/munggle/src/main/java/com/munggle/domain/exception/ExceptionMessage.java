ad99c2957046cc3ac662c02e735b71df95c5071f
이형우 <dlguddn3024@gmail.com>
2024-01-17 14:33:45 +0900

    :recycle: refactor: ExceptionMessage 변경

--- back/munggle/src/main/java/com/munggle/domain/exception/ExceptionMessage.java
+++ back/munggle/src/main/java/com/munggle/domain/exception/ExceptionMessage.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 public enum ExceptionMessage {
 
     USER_NOT_FOUND("회원을 찾을 수 없습니다."),
-    NICKNAME_ILLEGAL("닉네임에는 공백이 들어갈 수 없습니다."),
-    PASSWORD_ILLEGAL("비밀번호에는 공백이 들어갈 수 없습니다.");
+    NICKNAME_ILLEGAL("닉네임이 올바르지 않습니다."),
+    PASSWORD_ILLEGAL("비밀번호는 공백이 포함되지 않은 8 ~ 15자리 입니다.");
 
     private final String message;
 
