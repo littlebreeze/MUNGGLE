8163c8ca4ef26e1d849c6e7fdc2446cdea9c8e84
Yunji Lee <luj2868@gmail.com>
2024-01-19 15:33:59 +0900

    :sparkles: [BE] Feat: 게시글 목록 Dto 생성

---
+++ back/munggle/src/main/java/com/munggle/post/dto/PostListRequestDto.java
@@ -0,0 +1,11 @@
+package com.munggle.post.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class PostListRequestDto {
+
+    private Long userId;
+}
