821d9a9f80a706c50d66c465a3df9fb71e59ac01
Yunji Lee <luj2868@gmail.com>
2024-01-24 13:37:32 +0900

    :sparkles: [BE] feat: 저장해야할 파일이 여러개일 때 사용할 메소드 추가

--- back/munggle/src/main/java/com/munggle/image/service/FileS3UploadService.java
+++ back/munggle/src/main/java/com/munggle/image/service/FileS3UploadService.java
@@ -1,10 +1,15 @@
 package com.munggle.image.service;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.munggle.image.dto.FileInfoDto;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface FileS3UploadService {
-    String uploadFile(String uploadPath, MultipartFile multipartFile);
+    FileInfoDto uploadFile(String uploadPath, MultipartFile multipartFile);
+
+    List<FileInfoDto> uploadFlieList(String uploadPath, List<MultipartFile> multipartFiles);
 
     void removeFile(String fileName);
 }
--- back/munggle/src/main/java/com/munggle/image/service/FileS3UploadServiceImpl.java
+++ back/munggle/src/main/java/com/munggle/image/service/FileS3UploadServiceImpl.java
@@ -3,14 +3,18 @@ package com.munggle.image.service;
 //import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.*;
+import com.munggle.image.dto.FileInfoDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
+//import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -22,10 +26,34 @@ public class FileS3UploadServiceImpl implements FileS3UploadService {
     @Value("cloud.aws.s3.bucket")
     private String bucketName;
 
-    // 이미지 저장시 중복 방지를 위해 랜덤 이름 생성
+    /**
+     * 파일이름 랜덤생성 메소드
+     *
+     * @param extension
+     * @return
+     */
     private String generateRandomName(String extension) {
         String random = UUID.randomUUID().toString();
         return random + extension;
+    }
+
+    /**
+     * 확장자 제한하는 메소드
+     *
+     * @param extension
+     * @return
+     */
+    private String getContentType(String extension) {
+        switch (extension.toLowerCase()) {
+            case "jpeg":
+            case "jpg":
+            case "png":
+                return "image/" + extension;
+            case "mp4":
+                return "video/" + extension;
+            default:
+                throw new IllegalArgumentException("Unsupported file extension: " + extension);
+        }
     }
 
     /**
@@ -36,16 +64,16 @@ public class FileS3UploadServiceImpl implements FileS3UploadService {
      * @return : 이미지가 저장된 주소 (URL)
      */
     @Override
-    public String uploadFile(String uploadPath, MultipartFile multipartFile) {
+    public FileInfoDto uploadFile(String uploadPath, MultipartFile multipartFile) {
         String originName = multipartFile.getOriginalFilename(); //원본 이미지 이름
-        String ext = originName.substring(originName.lastIndexOf(".")); //확장자
+        String ext = StringUtils.getFilenameExtension(originName); //확장자
+
         String generatedName = uploadPath + "/" + generateRandomName(ext); //새로 생성된 이미지 이름
 
         ObjectMetadata metadata = new ObjectMetadata(); //메타데이터
-        metadata.setContentType("image/" + ext.substring(1)); //메타데이터 컨텐츠 타입 설정
-        // 확장자 제한 시 해당 코드 구현 필요
+        metadata.setContentType(getContentType(ext)); //메타데이터 컨텐츠 타입 설정
 
-        try {
+        try { //S3 버킷에 파일 업로드
             amazonS3Client.putObject(new PutObjectRequest(
                     bucketName, generatedName, multipartFile.getInputStream(), metadata
             ).withCannedAcl(CannedAccessControlList.PublicRead));
@@ -53,7 +81,30 @@ public class FileS3UploadServiceImpl implements FileS3UploadService {
             throw new RuntimeException(e);
         }
 
-        return amazonS3Client.getUrl(bucketName, generatedName).toString(); //데이터베이스에 저장할 이미지가 저장된 주소
+        String fileURL = amazonS3Client.getUrl(bucketName, generatedName).toString(); //데이터베이스에 저장할 이미지가 저장된 주소
+
+        return FileInfoDto.builder()
+                .fileName(generatedName)
+                .fileURL(fileURL)
+                .build();
+    }
+
+    /**
+     * 파일을 여러개 저장하는 메소드
+     *
+     * @param uploadPath
+     * @param multipartFiles
+     * @return
+     */
+    @Override
+    public List<FileInfoDto> uploadFlieList(String uploadPath, List<MultipartFile> multipartFiles) {
+
+        List<FileInfoDto> fileInfoDtos = new ArrayList<>();
+        for (MultipartFile file : multipartFiles) {
+            fileInfoDtos.add(uploadFile(uploadPath, file));
+        }
+
+        return fileInfoDtos;
     }
 
 
