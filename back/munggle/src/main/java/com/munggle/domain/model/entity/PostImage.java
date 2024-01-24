906bc12c3dff7760ef5b02bcb033cba96bcc2237
Yunji Lee <luj2868@gmail.com>
2024-01-24 17:33:56 +0900

    :hammer: [BE] fix: 게시글 엔티티 필드 수정, 어노테이션 추가

--- back/munggle/src/main/java/com/munggle/domain/model/entity/PostImage.java
+++ back/munggle/src/main/java/com/munggle/domain/model/entity/PostImage.java
@@ -1,11 +1,25 @@
 package com.munggle.domain.model.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
 @Entity
 @Table(name = "post_images")
+@DynamicUpdate
+@DynamicInsert
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
 public class PostImage {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "post_image_id")
     private Long id;
 
@@ -16,4 +30,6 @@ public class PostImage {
     @JoinColumn(name = "post_id", nullable = false)
     @ManyToOne(fetch = FetchType.LAZY)
     private Post post;
+
+    private Boolean isDeleted;
 }
