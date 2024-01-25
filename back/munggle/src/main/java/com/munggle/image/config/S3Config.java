56231b9af7bfccfc5e505ac987fd76d0559356a7
Yunji Lee <luj2868@gmail.com>
2024-01-25 17:58:37 +0900

    :hammer: [BE] fix: S3Config 코드 변경
    - @Value 어노테이션 SpEL로 변경

--- back/munggle/src/main/java/com/munggle/image/config/S3Config.java
+++ back/munggle/src/main/java/com/munggle/image/config/S3Config.java
@@ -10,11 +10,11 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
-    @Value("cloud.aws.credentials.accessKey")
+    @Value("${cloud.aws.credentials.accessKey}")
     private String accessKey;
-    @Value("cloud.aws.credentials.secretKey")
+    @Value("${cloud.aws.credentials.secretKey}")
     private String secretKey;
-    @Value("cloud.aws.region.static")
+    @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean
