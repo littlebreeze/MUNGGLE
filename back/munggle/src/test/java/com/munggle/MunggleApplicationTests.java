d823cbd444e41cbb10121d215b6a12c4620fadf8
dlguddn3024 <dlguddn3024@gmail.com>
2024-01-27 00:46:55 +0900

    :wrench:  [BE]chore: 빌드 오탈자 수정

--- back/munggle/src/test/java/com/munggle/MunggleApplicationTests.java
+++ back/munggle/src/test/java/com/munggle/MunggleApplicationTests.java
@@ -2,6 +2,7 @@ package com.munggle;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("prod")
 @SpringBootTest
