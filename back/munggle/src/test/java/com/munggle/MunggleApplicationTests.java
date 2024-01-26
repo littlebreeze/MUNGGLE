1d96ecccdaee4f1433f7f3912ec79dd7c752a1c6
dlguddn3024 <dlguddn3024@gmail.com>
2024-01-27 00:42:34 +0900

    :green_heart:  [BE] docs: 스크립트 수정

--- back/munggle/src/test/java/com/munggle/MunggleApplicationTests.java
+++ back/munggle/src/test/java/com/munggle/MunggleApplicationTests.java
@@ -3,6 +3,7 @@ package com.munggle;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@ActiveProfiles("prod")
 @SpringBootTest
 class MunggleApplicationTests {
 
