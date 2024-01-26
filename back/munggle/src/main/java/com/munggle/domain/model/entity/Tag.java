217f05357e5170074c2d07eda918e9f18653521a
Yunji Lee <luj2868@gmail.com>
2024-01-26 15:44:02 +0900

    :sparkles: [BE] feat: 해시태그 엔티티 수정

--- back/munggle/src/main/java/com/munggle/domain/model/entity/Tag.java
+++ back/munggle/src/main/java/com/munggle/domain/model/entity/Tag.java
@@ -1,6 +1,28 @@
 package com.munggle.domain.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+@Entity
+@Table(name = "tags")
+@DynamicInsert
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Builder
 public class Tag {
+
+    @Id
     private Long tagId;
+
+    @NotNull
+    @NotBlank
     private String tagNm;
 }
