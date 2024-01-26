a021acc3f81e7c0bc849ca072a8dc9c4decc7522
dlguddn3024 <dlguddn3024@gmail.com>
2024-01-27 04:24:38 +0900

    :green_heart:  [BE] docs: 스크립트 수정

--- back/munggle/deploy/start-app.sh
+++ back/munggle/deploy/start-app.sh
@@ -2,10 +2,11 @@
 
 profiles="${ENV}, common"
 
-base_config="classpath:/application.yaml"
-mounted_configs=$(ls ./config/*.yaml | paste -sd, -)
-configs="$base_config, $mounted_configs"
+#base_config="classpath:/application.yaml"
+#mounted_configs=$(ls ./config/*.yaml | paste -sd, -)
+#configs="$base_config, $mounted_configs"
 
 java -jar ./app.jar \
-  --spring.profiles.active="$profiles" \
-  --spring.config.location="$configs"
+#  --spring.config.location="$configs" \
+  --spring.profiles.active="$profiles"
+
