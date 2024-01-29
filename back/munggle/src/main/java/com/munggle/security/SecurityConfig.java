1177c8b6d1822bc3b01c71ebd3005d9cc6322d58
이형우 <dlguddn3024@gmail.com>
2024-01-29 13:35:24 +0900

    :lock:  [BE]chore: 크로스오버 설정 수정

--- back/munggle/src/main/java/com/munggle/security/SecurityConfig.java
+++ back/munggle/src/main/java/com/munggle/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.munggle.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -33,6 +34,7 @@ public class SecurityConfig {
 //
 //        return http.build();
         http
+                .cors(Customizer.withDefaults())
                 .csrf().disable()
                 .authorizeRequests()
                 .anyRequest()
