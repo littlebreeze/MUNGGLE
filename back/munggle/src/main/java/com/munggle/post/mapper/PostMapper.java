8e916b05f35a9d7fc07672638cdb4e7e8f8866bc
Yunji Lee <luj2868@gmail.com>
2024-01-29 16:58:14 +0900

    :hammer: [BE] fix: 데이터 insert시 isDeleted 필드 false로 설정
    - PostMapper의 toPostImageEntity 메소드 변경

--- back/munggle/src/main/java/com/munggle/post/mapper/PostMapper.java
+++ back/munggle/src/main/java/com/munggle/post/mapper/PostMapper.java
@@ -43,6 +43,7 @@ public class PostMapper {
                 .imageName(file.getFileName())
                 .imageURL(file.getFileURL())
                 .post(post)
+                .isDeleted(false)
                 .build();
     }
 
