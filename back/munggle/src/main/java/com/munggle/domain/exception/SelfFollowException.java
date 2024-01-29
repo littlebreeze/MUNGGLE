15a594fed829f6d663eb11f50a41a55d31447f19
이형우 <dlguddn3024@gmail.com>
2024-01-29 10:49:37 +0900

    :truck:  [BE]rename: 유저 간 상호작용 예외 클래스명 수정

--- back/munggle/src/main/java/com/munggle/domain/exception/SelfFollowException.java
+++
@@ -1,7 +0,0 @@
-package com.munggle.domain.exception;
-
-public class SelfFollowException extends RuntimeException{
-    public SelfFollowException(ExceptionMessage msg) {
-        super(msg.getMessage());
-    }
-}
