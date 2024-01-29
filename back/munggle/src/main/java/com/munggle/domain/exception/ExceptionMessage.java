cc073ca261f083b8bb0ae07c09361341c66770ce
이형우 <dlguddn3024@gmail.com>
2024-01-29 11:30:23 +0900

    :recycle:  [BE]refactor: 차단 메세지 수정

--- back/munggle/src/main/java/com/munggle/domain/exception/ExceptionMessage.java
+++ back/munggle/src/main/java/com/munggle/domain/exception/ExceptionMessage.java
@@ -8,10 +8,10 @@ public enum ExceptionMessage {
     USER_NOT_FOUND("회원을 찾을 수 없습니다."),
     NICKNAME_ILLEGAL("닉네임이 올바르지 않습니다."),
     PASSWORD_ILLEGAL("비밀번호는 공백이 포함되지 않은 8 ~ 15자리 입니다."),
-    SELF_FOLLOW("자기 자신을 팔로우할 수 없습니다."),
-    SELF_BLOCK("자기 자신을 차단할 수 없습니다."),
-    BLOCK_NOT_FOUND("차단을 찾을 수 없습니다"),
-    FOLLOW_NOT_FOUND("팔로우를 찾을 수 없습니다."),
+    SELF_FOLLOW("자신을 팔로우할 수 없습니다."),
+    SELF_BLOCK("자신을 차단할 수 없습니다."),
+    BLOCK_NOT_FOUND("차단된 회원을 찾을 수 없습니다"),
+    FOLLOW_NOT_FOUND("팔로우한 회원을 찾을 수 없습니다."),
     WALK_NOT_FOUND("산책 기록을 찾을 수 없습니다."),
     WALK_LOG_NOT_FOUND("산책 로그를 찾을 수 없습니다.");
 
