1552a14117dae2f9d60642c73f6851a268544087
이형우 <dlguddn3024@gmail.com>
2024-01-29 11:28:07 +0900

    :sparkles:  [BE]feat: 차단 해제 기능 예외 처리 구현

--- back/munggle/src/main/java/com/munggle/domain/exception/ExceptionMessage.java
+++ back/munggle/src/main/java/com/munggle/domain/exception/ExceptionMessage.java
@@ -10,6 +10,7 @@ public enum ExceptionMessage {
     PASSWORD_ILLEGAL("비밀번호는 공백이 포함되지 않은 8 ~ 15자리 입니다."),
     SELF_FOLLOW("자기 자신을 팔로우할 수 없습니다."),
     SELF_BLOCK("자기 자신을 차단할 수 없습니다."),
+    BLOCK_NOT_FOUND("차단을 찾을 수 없습니다"),
     FOLLOW_NOT_FOUND("팔로우를 찾을 수 없습니다."),
     WALK_NOT_FOUND("산책 기록을 찾을 수 없습니다."),
     WALK_LOG_NOT_FOUND("산책 로그를 찾을 수 없습니다.");
