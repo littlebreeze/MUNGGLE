35ae02f21cade8b6e393604a302f64e33284ef33
Yunji Lee <luj2868@gmail.com>
2024-01-31 14:05:43 +0900

    :sparkles: [BE] feat: redis에 저장할 객체 생성

---
+++ back/munggle/src/main/java/com/munggle/domain/model/entity/UserRecentTagCache.java
@@ -0,0 +1,22 @@
+package com.munggle.domain.model.entity;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+
+@Getter @Builder
+@AllArgsConstructor
+@RedisHash(value = "user_tag")
+public class UserRecentTagCache {
+
+    @Id
+    private Long id;
+
+    private Long userId;
+
+    private Long postId;
+}
+
