6ac036ea1d7ef19573f4da48349bab7ff3cc74ab
Yunji Lee <luj2868@gmail.com>
2024-02-01 18:09:36 +0900

    :hammer: [BE] fix: RedisConfig update

--- back/munggle/src/main/java/com/munggle/util/config/RedisConfig.java
+++ back/munggle/src/main/java/com/munggle/util/config/RedisConfig.java
@@ -1,41 +1,53 @@
 package com.munggle.util.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-//import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-//import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableRedisRepositories
+@Slf4j
 public class RedisConfig {
 
     private final RedisProperties redisProperties;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+        lettuceConnectionFactory.setHostName(redisProperties.getHost());
+        lettuceConnectionFactory.setPort(redisProperties.getPort());
+        lettuceConnectionFactory.setPassword(redisProperties.getPassword());
+        return lettuceConnectionFactory;
     }
+
 
     /**
      java Object를 redis에 저장하는 경우 사용
      - StringRedisSerializer를 사용해서 String값에 대한 적용도 가능하나
      default는 JdkSerializationRedisSerializer
-     */ /*
+     */
     @Bean
-    public RedisTemplate<?, ?> redisTemplate() {
-        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+//    public RedisTemplate<?, ?>  redisTemplate() {
+    public RedisTemplate<String, Object>  redisTemplate() {
+//        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
         return redisTemplate;
-    } */
+    }
 
     /**
      * 일반적인 String 값을 key, value로 사용하는 경우 사용
