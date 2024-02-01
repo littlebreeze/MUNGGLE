5e0ba756d6206752e25d2474570b3b7c1c6f6cf0
Yunji Lee <luj2868@gmail.com>
2024-02-01 18:11:22 +0900

    :hammer: [BE] fix: UserRecentTagCache 필드 변경
    - @Indexed 어노테이션 추가
    - id: Long -> String 형 변환

--- back/munggle/src/main/java/com/munggle/domain/model/entity/UserRecentTagCache.java
+++ back/munggle/src/main/java/com/munggle/domain/model/entity/UserRecentTagCache.java
@@ -1,22 +1,32 @@
 package com.munggle.domain.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+import java.io.Serializable;
 
 @Getter @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 @RedisHash(value = "user_tag")
-public class UserRecentTagCache {
+public class UserRecentTagCache implements Serializable {
 
     @Id
-    private Long id;
+    @Indexed
+    private String id;
 
+    @Indexed
     private Long userId;
 
+    @Indexed
     private Long tagId;
 }
 
--- back/munggle/src/main/java/com/munggle/post/repository/UserRecentTagCacheRepository.java
+++ back/munggle/src/main/java/com/munggle/post/repository/UserRecentTagCacheRepository.java
@@ -1,17 +1,14 @@
 package com.munggle.post.repository;
 
 import com.munggle.domain.model.entity.UserRecentTagCache;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRecentTagCacheRepository extends CrudRepository<UserRecentTagCache, Long> {
-    List<UserRecentTagCache> findByUserId(Long userId, PageRequest pageRequest);
+public interface UserRecentTagCacheRepository extends CrudRepository<UserRecentTagCache, String> {
+    List<UserRecentTagCache> findByUserId(Long userId);
 
-    @Override
-    List<UserRecentTagCache> findAll();
 
     Optional<UserRecentTagCache> findByUserIdAndTagId(Long userId, Long tagId);
 }
