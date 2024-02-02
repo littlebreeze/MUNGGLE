faa912b8f3f1441442ef267e752e7d8c3015d20b
Yunji Lee <luj2868@gmail.com>
2024-02-02 11:21:53 +0900

    :sprakles: [BE] feat: ExceptionMessage 추가

--- back/munggle/src/main/java/com/munggle/domain/exception/ExceptionMessage.java
+++ back/munggle/src/main/java/com/munggle/domain/exception/ExceptionMessage.java
@@ -13,7 +13,16 @@ public enum ExceptionMessage {
     BLOCK_NOT_FOUND("차단된 회원을 찾을 수 없습니다"),
     FOLLOW_NOT_FOUND("팔로우한 회원을 찾을 수 없습니다."),
     WALK_NOT_FOUND("산책 기록을 찾을 수 없습니다."),
+    MAIL_NOT_VALID("이메일이 유효하지 않습니다."),
+    EMAIL_DUPLICATE("이미 가입된 이메일입니다."),
     WALK_LOG_NOT_FOUND("산책 로그를 찾을 수 없습니다."),
+    POST_NOT_FOUND("게시글을 찾을 수 없습니다."),
+    EXTENSION_ILLEGAL("지원하지 않는 파일 확장자입니다."),
+    PASSWORD_NOT_CONFIRM("새 비밀번호와 비밀번호 확인이 일치하지 않습니다."),
+    DOG_NOT_FOUND("반려견 정보를 찾을 수 없습니다."),
+    MATCHING_CHARACTER_NOT_FOUND("매칭 특성이 등록되지 않았습니다."),
+    MATCHING_IS_NOT_ON("매칭 옵션이 켜지지 않았습니다."),
+    NOT_YOUR_DOG("당신의 반려견이 아닙니다."),
     TAG_NOT_FOUND("해당 태그를 찾을 수 없습니다.");
 
 
