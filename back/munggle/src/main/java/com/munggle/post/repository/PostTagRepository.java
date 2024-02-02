d67b8bd1a32e58bd9a4ac735ebb468a98dd12646
Yunji Lee <luj2868@gmail.com>
2024-02-02 12:39:18 +0900

    :hammer: [BE] fix: repository 어노테이션 추가

--- back/munggle/src/main/java/com/munggle/post/repository/PostTagRepository.java
+++ back/munggle/src/main/java/com/munggle/post/repository/PostTagRepository.java
@@ -4,9 +4,11 @@ import com.munggle.domain.model.entity.Post;
 import com.munggle.domain.model.entity.PostTag;
 import com.munggle.domain.model.entity.PostTagId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PostTagRepository extends JpaRepository<PostTag, PostTagId> {
     List<PostTag> findAllByPost(Post post);
 }
