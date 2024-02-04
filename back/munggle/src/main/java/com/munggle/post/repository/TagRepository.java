1ca72454e35f357c66072ecd02eeffc0d332e52f
yunjiL <luj2868@gmail.com>
2024-02-04 16:06:04 +0900

    :sparkles: [BE] feat: tag 검색 기능 구현

--- back/munggle/src/main/java/com/munggle/post/repository/TagRepository.java
+++ back/munggle/src/main/java/com/munggle/post/repository/TagRepository.java
@@ -2,10 +2,16 @@ package com.munggle.post.repository;
 
 import com.munggle.domain.model.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
     Optional<Tag> findByTagNm(String hashtag);
+
+    @Query("select t from Tag t where t.tagNm like concat('%', :word, '%')")
+    List<Tag> searchByTagNm(@Param("word") String word);
 }
