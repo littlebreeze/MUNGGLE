c9fdd728e80d7819111aebb2a283e489e8fc8ded
dlguddn3024 <dlguddn3024@gmail.com>
2024-02-06 03:46:52 +0900

    :sparkles:  [BE]feat: OpenId 로그인을 위한 서비스 구현

---
+++ back/munggle/src/main/java/com/munggle/user/service/OidUserService.java
@@ -0,0 +1,10 @@
+package com.munggle.user.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+
+public interface OidUserService extends OAuth2UserService<OidcUserRequest, OidcUser> {
+
+}
