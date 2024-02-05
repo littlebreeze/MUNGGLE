d813fcfc9ea86bb4fb81e98fb6df62b0c076042a
Yunji Lee <luj2868@gmail.com>
2024-02-05 15:12:30 +0900

    :sparkles: [BE] feat: ScrapRepository 어노테이션 추가

--- back/munggle/src/main/java/com/munggle/post/repository/ScrapRepository.java
+++ back/munggle/src/main/java/com/munggle/post/repository/ScrapRepository.java
@@ -3,6 +3,8 @@ package com.munggle.post.repository;
 import com.munggle.domain.model.entity.Scrap;
 import com.munggle.domain.model.entity.ScrapId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ScrapRepository extends JpaRepository<Scrap, ScrapId> {
 }
