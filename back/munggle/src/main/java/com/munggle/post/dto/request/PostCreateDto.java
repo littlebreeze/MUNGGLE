1f29c9dd7ecb3afea95f077702ce733785034f04
Yunji Lee <luj2868@gmail.com>
2024-02-06 17:55:52 +0900

    :sparkles: [BE] feat: @JsonProperty 추가

--- back/munggle/src/main/java/com/munggle/post/dto/request/PostCreateDto.java
+++ back/munggle/src/main/java/com/munggle/post/dto/request/PostCreateDto.java
@@ -1,5 +1,7 @@
 package com.munggle.post.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -18,18 +20,21 @@ public class PostCreateDto {
 
     @Size(max = 100)
     @NotBlank
+    @JsonProperty("postTitle")
     private String postTitle;
 
     @Size(max = 500)
+    @JsonProperty("postContent")
     private String postContent;
 
     // 포스트 이미지
-    @Builder.Default
-    private List<MultipartFile> images = new ArrayList<>();
+    private List<MultipartFile> images;
 
     // 해시태그
     @Builder.Default
+    @JsonProperty("hashtags")
     private List<String> hashtags = new ArrayList<>();
 
+    @JsonProperty("isPrivate")
     private Boolean isPrivate;
 }
