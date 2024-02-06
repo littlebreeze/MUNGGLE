bb5777c3f99fb7b594cfc0a3fa919cd94be15a59
이형우 <dlguddn3024@gmail.com>
2024-02-06 13:38:16 +0900

    :poop:  [BE]fix: 이메일 유효기간 확인 로직 구현

--- back/munggle/src/main/java/com/munggle/emailverification/EmailVerificationRepository.java
+++ back/munggle/src/main/java/com/munggle/emailverification/EmailVerificationRepository.java
@@ -1,10 +1,12 @@
 package com.munggle.emailverification;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface EmailVerificationRepository extends JpaRepository<EmailVerification, Long> {
+    @Query(value = "SELECT * FROM email_verification WHERE email = :email AND expiration_time >= NOW() - INTERVAL '3 minutes' ORDER BY expiration_time DESC LIMIT 1", nativeQuery = true)
     Optional<EmailVerification> findByEmail(String email);
     void deleteByEmail(String email);
 }
