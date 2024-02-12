7518b5d3f75ada873ea250dbfa748d90936c95e0
yunjiL <luj2868@gmail.com>
2024-02-12 13:59:49 +0900

    :sparkles: [BE] feat: 알람 detail mapper 함수 생성

--- back/munggle/src/main/java/com/munggle/alarm/mapper/AlarmMapper.java
+++ back/munggle/src/main/java/com/munggle/alarm/mapper/AlarmMapper.java
@@ -1,5 +1,7 @@
 package com.munggle.alarm.mapper;
 
+import com.munggle.alarm.dto.AlarmDetailDto;
+import com.munggle.domain.model.entity.Alarm;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -7,4 +9,13 @@ import org.springframework.stereotype.Component;
 @Component
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AlarmMapper {
+    public static AlarmDetailDto toAlarmDetailDto(Alarm alarm) {
+        return AlarmDetailDto.builder()
+                .alarmId(alarm.getId())
+                .alarmType(alarm.getAlarmType().toString())
+                .fromUserId(alarm.getFromUser().getId())
+                .targetId(alarm.getTargetId())
+                .isChecked(alarm.getIsChecked())
+                .build();
+    }
 }
