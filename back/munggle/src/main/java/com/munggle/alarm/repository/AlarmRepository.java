dc56d5d69c201801a61336398092b74e002d120b
yunjiL <luj2868@gmail.com>
2024-02-12 14:01:52 +0900

    :sparkles: [BE] feat: 알람 repository 함수 추가

--- back/munggle/src/main/java/com/munggle/alarm/repository/AlarmRepository.java
+++ back/munggle/src/main/java/com/munggle/alarm/repository/AlarmRepository.java
@@ -1,10 +1,15 @@
 package com.munggle.alarm.repository;
 
+import com.munggle.alarm.dto.AlarmDetailDto;
 import com.munggle.domain.model.entity.Alarm;
+import com.munggle.domain.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
 
+    List<Alarm> findByToUserIdAndIsDeletedFalse(User toUser);
 }
