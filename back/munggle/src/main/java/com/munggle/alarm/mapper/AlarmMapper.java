56e515a7fc8d9cf5d01da89880b8e76e7942d1e7
yunjiL <luj2868@gmail.com>
2024-02-12 15:31:23 +0900

    :sparkles: [BE] feat: 알람 생성 기능 구현

--- back/munggle/src/main/java/com/munggle/alarm/mapper/AlarmMapper.java
+++ back/munggle/src/main/java/com/munggle/alarm/mapper/AlarmMapper.java
@@ -1,7 +1,10 @@
 package com.munggle.alarm.mapper;
 
+import com.munggle.alarm.dto.AlarmCreateDto;
 import com.munggle.alarm.dto.AlarmDetailDto;
 import com.munggle.domain.model.entity.Alarm;
+import com.munggle.domain.model.entity.User;
+import com.munggle.domain.model.entity.type.AlarmType;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -9,6 +12,18 @@ import org.springframework.stereotype.Component;
 @Component
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AlarmMapper {
+
+    public static Alarm toEntity(AlarmCreateDto alarm, User from, User to, AlarmType type) {
+
+        return Alarm.builder()
+                .alarmType(type)
+                .fromUser(from)
+                .toUser(to)
+                .targetId(alarm.getTarget())
+                .isChecked(false)
+                .build();
+    }
+
     public static AlarmDetailDto toAlarmDetailDto(Alarm alarm) {
         return AlarmDetailDto.builder()
                 .alarmId(alarm.getId())
@@ -18,4 +33,5 @@ public class AlarmMapper {
                 .isChecked(alarm.getIsChecked())
                 .build();
     }
+
 }
--- back/munggle/src/main/java/com/munggle/alarm/service/AlarmServiceImpl.java
+++ back/munggle/src/main/java/com/munggle/alarm/service/AlarmServiceImpl.java
@@ -1,25 +1,26 @@
 package com.munggle.alarm.service;
 
 import com.munggle.alarm.dto.AlarmCreateDto;
-import com.munggle.alarm.dto.AlarmDetailDto;
 import com.munggle.alarm.dto.AlarmListDto;
 import com.munggle.alarm.mapper.AlarmMapper;
 import com.munggle.alarm.repository.AlarmRepository;
 import com.munggle.domain.exception.AlarmNotFoundException;
+import com.munggle.domain.exception.IllegalAlarmTypeException;
 import com.munggle.domain.exception.UserNotFoundException;
 import com.munggle.domain.model.entity.Alarm;
 import com.munggle.domain.model.entity.User;
+import com.munggle.domain.model.entity.type.AlarmType;
 import com.munggle.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static com.munggle.domain.exception.ExceptionMessage.ALARM_NOT_FOUND;
-import static com.munggle.domain.exception.ExceptionMessage.USER_NOT_FOUND;
+import static com.munggle.domain.exception.ExceptionMessage.*;
 
 @Service @Slf4j
 @RequiredArgsConstructor
@@ -44,10 +45,37 @@ public class AlarmServiceImpl implements AlarmService{
                 .build();
     }
 
+    // === 알림 타입을 지정해주는 메서드 === //
+    private AlarmType getAlarmType(String type) {
+        switch (type) {
+            case "POST":
+                return AlarmType.POST;
+            case "LIKE":
+                return AlarmType.LIKE;
+            case "COMMENT":
+                return AlarmType.COMMENT;
+            case "DM":
+                return AlarmType.DM;
+            case "FOLLOW":
+                return AlarmType.FOLLOW;
+            default:
+                throw new IllegalAlarmTypeException(ALARM_TYPE_ILLEGAL);
+        }
+    }
+
     @Override
     @Transactional
     public void InsetAlarm(AlarmCreateDto alarmCreateDto) {
 
+        AlarmType type = getAlarmType(alarmCreateDto.getAlarmType());
+        User fromUser = userRepository.findByIdAndIsEnabledTrue(alarmCreateDto.getFromUserId())
+                .orElseThrow(() -> new UserNotFoundException(USER_NOT_FOUND));
+        User toUser = userRepository.findByIdAndIsEnabledTrue(alarmCreateDto.getToUserId())
+                .orElseThrow(() -> new UserNotFoundException(USER_NOT_FOUND));
+
+        Alarm alarm = AlarmMapper.toEntity(alarmCreateDto, fromUser, toUser, type);
+
+        alarmRepository.save(alarm);
     }
 
     @Override
