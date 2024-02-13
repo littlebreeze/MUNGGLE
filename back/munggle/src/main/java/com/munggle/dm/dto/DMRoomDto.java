77f307725fe40ae81eee9894e7e5f3518e95eb57
이형우 <dlguddn3024@gmail.com>
2024-02-13 14:32:28 +0900

    :recycle:  [BE]refactor: dm방 dto 필요한 필드 추가

--- back/munggle/src/main/java/com/munggle/dm/dto/DMRoomDto.java
+++ back/munggle/src/main/java/com/munggle/dm/dto/DMRoomDto.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,6 +14,9 @@ import lombok.NoArgsConstructor;
 public class DMRoomDto {
 
     private Long roomId;
+    private Long otherUserId;
     private String userNickname;
     private String profileImage;
+    private String lastContent;
+    private LocalDateTime lastSendTime;
 }
--- back/munggle/src/main/java/com/munggle/dm/service/DMRoomServiceImpl.java
+++ back/munggle/src/main/java/com/munggle/dm/service/DMRoomServiceImpl.java
@@ -2,12 +2,14 @@ package com.munggle.dm.service;
 
 import com.munggle.dm.dto.DMRoomDto;
 import com.munggle.dm.repository.DMRoomRepository;
+import com.munggle.domain.model.entity.ChatMessage;
 import com.munggle.domain.model.entity.User;
 import com.munggle.domain.model.entity.UserImage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -23,21 +25,34 @@ public class DMRoomServiceImpl implements DMRoomService {
     public List<DMRoomDto> getDMRoomList(Long id) {
         return dmRoomRepository.findByUsers_Id(id)
                 .stream()
-                .map(room -> DMRoomDto.builder()
-                        .roomId(room.getId())
-                        .userNickname(room.getUsers().stream()
-                                .filter(user -> !user.getId().equals(id))
-                                .findFirst()
-                                .map(User::getNickname)
-                                .orElse(null))
-                        .profileImage(room.getUsers().stream()
-                                .filter(user -> !user.getId().equals(id))
-                                .findFirst()
-                                .map(user -> Optional.ofNullable(user.getProfileImage())
-                                        .map(UserImage::getImageURL)
-                                        .orElse(null))
-                                .orElse(null))
-                        .build())
+                .map(room -> {
+                    ChatMessage lastMessage = room.getChatMessages().stream()
+                            .max(Comparator.comparing(ChatMessage::getCreateAt))
+                            .orElse(null);
+
+                    return DMRoomDto.builder()
+                            .roomId(room.getId())
+                            .otherUserId(room.getUsers().stream()
+                                    .filter(user -> !user.getId().equals(id))
+                                    .findFirst()
+                                    .map(User::getId)
+                                    .orElse(null))
+                            .userNickname(room.getUsers().stream()
+                                    .filter(user -> !user.getId().equals(id))
+                                    .findFirst()
+                                    .map(User::getNickname)
+                                    .orElse(null))
+                            .profileImage(room.getUsers().stream()
+                                    .filter(user -> !user.getId().equals(id))
+                                    .findFirst()
+                                    .map(user -> Optional.ofNullable(user.getProfileImage())
+                                            .map(UserImage::getImageURL)
+                                            .orElse(null))
+                                    .orElse(null))
+                            .lastContent(lastMessage != null ? lastMessage.getContent() : null)
+                            .lastSendTime(lastMessage != null ? lastMessage.getCreateAt() : null)
+                            .build();
+                })
                 .collect(Collectors.toList());
     }
 }
