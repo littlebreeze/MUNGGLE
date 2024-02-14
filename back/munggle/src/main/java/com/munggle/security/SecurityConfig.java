5ada447ca03ae59fe2a02c9f9080ec8d5972aa30
이형우 <dlguddn3024@gmail.com>
2024-02-14 16:44:40 +0900

    :recycle:  [BE]refactor: 체인 및 세션 재설정

--- back/munggle/src/main/java/com/munggle/security/SecurityConfig.java
+++ back/munggle/src/main/java/com/munggle/security/SecurityConfig.java
@@ -35,30 +35,56 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //체인 완성본
+//        http
+//                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .sessionManagement(
+//                        sessionManagement -> sessionManagement
+//                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .authorizeRequests(
+//                        registry -> registry.requestMatchers(HttpMethod.POST, "/users").permitAll()
+//                                .requestMatchers("/users/emails/verification-requests").permitAll()
+//                                .requestMatchers("/users/emails/verifications").permitAll()
+//                                .requestMatchers("/users/**").hasAnyRole("ADMIN", "MEMBER")
+//                                .requestMatchers("/userpages/**").hasAnyRole("ADMIN", "MEMBER")
+//                                .requestMatchers("/alarms/**").hasAnyRole("ADMIN", "MEMBER")
+//                                .requestMatchers("/blocks/**").hasAnyRole("ADMIN", "MEMBER")
+//                                .requestMatchers("/comments/**").hasAnyRole("ADMIN", "MEMBER")
+//                                .requestMatchers("/dogs/**").hasAnyRole("ADMIN", "MEMBER")
+//                                .requestMatchers("/dog-match/**").hasAnyRole("ADMIN", "MEMBER")
+//                                .requestMatchers("/dogs/**").hasAnyRole("ADMIN", "MEMBER")
+//                                .requestMatchers("/follows/**").hasAnyRole("ADMIN", "MEMBER")
+//                                .requestMatchers("/posts/**").hasAnyRole("ADMIN", "MEMBER")
+//                                .requestMatchers("/search/**").hasAnyRole("ADMIN", "MEMBER")
+//                                .requestMatchers("/walks/**").hasAnyRole("ADMIN", "MEMBER")
+//                                .requestMatchers("/message/**").hasAnyRole("ADMIN", "MEMBER")
+//                                .anyRequest().authenticated()
+//                )
+//                .formLogin(
+//                        configure -> configure.successHandler(new LoginAuthenticationSuccessHandler(jwtProvider))
+//                                .failureHandler(new LoginAuthenticationFailureHandler()))
+//                .exceptionHandling(
+//                        configurer -> configurer.accessDeniedHandler(new JwtAccessDeniedHandler())
+//                                .authenticationEntryPoint(new JwtAuthenticationEntryPoint())
+//                )
+//                .oauth2Login(
+//                        configure -> configure.successHandler(new LoginAuthenticationSuccessHandler(jwtProvider))
+//                                .failureHandler(new LoginAuthenticationFailureHandler())
+//                )
+//
+//                .exceptionHandling(
+//                        configurer -> configurer.accessDeniedHandler(new JwtAccessDeniedHandler())
+//                                .authenticationEntryPoint(new JwtAuthenticationEntryPoint())
+//                )
+//                .addFilterBefore(new JwtAuthenticationFilter(jwtProvider),
+//                        UsernamePasswordAuthenticationFilter.class);
+
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(
-                        sessionManagement -> sessionManagement
-                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeRequests(
-                        registry -> registry.requestMatchers(HttpMethod.POST, "/users").permitAll()
-                                .requestMatchers("/users/emails/verification-requests").permitAll()
-                                .requestMatchers("/users/emails/verifications").permitAll()
-                                .requestMatchers("/users/**").hasAnyRole("ADMIN", "MEMBER")
-                                .requestMatchers("/userpages/**").hasAnyRole("ADMIN", "MEMBER")
-                                .requestMatchers("/alarms/**").hasAnyRole("ADMIN", "MEMBER")
-                                .requestMatchers("/blocks/**").hasAnyRole("ADMIN", "MEMBER")
-                                .requestMatchers("/comments/**").hasAnyRole("ADMIN", "MEMBER")
-                                .requestMatchers("/dogs/**").hasAnyRole("ADMIN", "MEMBER")
-                                .requestMatchers("/dog-match/**").hasAnyRole("ADMIN", "MEMBER")
-                                .requestMatchers("/dogs/**").hasAnyRole("ADMIN", "MEMBER")
-                                .requestMatchers("/follows/**").hasAnyRole("ADMIN", "MEMBER")
-                                .requestMatchers("/posts/**").hasAnyRole("ADMIN", "MEMBER")
-                                .requestMatchers("/search/**").hasAnyRole("ADMIN", "MEMBER")
-                                .requestMatchers("/walks/**").hasAnyRole("ADMIN", "MEMBER")
-                                .requestMatchers("/message/**").hasAnyRole("ADMIN", "MEMBER")
-                                .anyRequest().authenticated()
+                        registry -> registry.anyRequest().permitAll()
                 )
                 .formLogin(
                         configure -> configure.successHandler(new LoginAuthenticationSuccessHandler(jwtProvider))
@@ -78,6 +104,7 @@ public class SecurityConfig {
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtProvider),
                         UsernamePasswordAuthenticationFilter.class);
+
 
         return http.build();
     }
